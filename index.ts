f6977b5ed57104123656106bdf3cdb9f807569a3
Chris Hanes <cah4i@yahoo.com>
2017-11-10 23:19:27 -0500

    start

--- index.ts
+++ index.ts
@@ -18,12 +18,27 @@ import * as _ from 'lodash';
 
 class Command {
     constructor(private num: number) {
-
+        this.execute = this.execute.bind(this);
+        console.log(num);
     }
 
     execute() {
-        console.log(`Executing ${this.num}`);
-        return Rx.Observable.empty();
+        const num = this.num;
+        return Rx.Observable.create(function (observer: Rx.Observer<Command>) {
+            const x = Math.floor(Math.random() * 100);
+            console.log('x',x);
+            setTimeout(() => {
+                if (x < 55) {
+                    console.log(`Executing ${num}`);
+                    observer.complete();
+                } else {
+                    observer.error(new Error(`Error with ${num}: ${x}`));
+                }
+            }, 1000 + x);
+        });
+
+
+        //return Rx.Observable.empty();
         // return observable that completes?
     }
 }
@@ -54,11 +69,13 @@ const commands = [new Command(0), new Command(1), new Command(2)];
 
 //const example = Rx.Observable.concat(getObservable(1), getObservable(2), getObservable(3), getObservable(4));
 
-const example = Rx.Observable.concat(_.map(commands, (com: Command) => (com.execute())));
+const example = Rx.Observable.concat(..._.map(commands, (com: Command) => (com.execute())));
+
+//const example = Rx.Observable.concat(...commands);
 
 //output: 1,2,3,4,5,6
 const subscribe = example.subscribe((val) => {
-    console.log('got', val);
+    console.log('got2', val);
 }, (err) => {
     console.log(err.message);
 });
