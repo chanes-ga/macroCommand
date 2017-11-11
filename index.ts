c398dd31fd6e14fe3c767c12652d46febfc43044
Chris Hanes <cah4i@yahoo.com>
2017-11-10 22:11:08 -0500

    start

--- index.ts
+++ index.ts
@@ -1,26 +1,27 @@
 import * as Rx from 'rxjs';
 
 
-const hello = Rx.Observable.create(function (observer: Rx.Observer<any>) {
-    observer.next('Hello');
 
-    observer.next('World');
-    const x = Math.random();
-    console.log(x);
-    if (x < 0.5) {
-        observer.complete();
-    } else {
-        observer.error(new Error("bad"));
-    }
-});
+function getObservable(num:number) {
+    return Rx.Observable.create(function (observer: Rx.Observer<any>) {
+        observer.next(num);
+        const x = Math.random();
+        console.log(x);
+        if (x < 0.5) {
+            observer.complete();
+        } else {
+            observer.error(new Error(`Error with ${num}`));
+        }
+    });
+}
 
 //emits 1,2,3
 const sourceOne = Rx.Observable.of(1, 2, 3);
 //emits 4,5,6
 const sourceTwo = Rx.Observable.of(4, 5, 6);
 //emit values from sourceOne, when complete, subscribe to sourceTwo
-const example = Rx.Observable.concat(sourceOne, hello, sourceTwo);
+const example = Rx.Observable.concat(getObservable(1),getObservable(2), getObservable(3),getObservable(4));
 //output: 1,2,3,4,5,6
-const subscribe = example.subscribe(val => console.log('Example: Basic concat:', val), (err) => {
-    console.log('b', err);
+const subscribe = example.subscribe(val => console.log('concat:', val), (err) => {
+    console.log('Got error', err);
 });
