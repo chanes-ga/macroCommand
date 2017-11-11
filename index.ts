34537474cad36e02ebd062f819c09cc96eae8717
Chris Hanes <cah4i@yahoo.com>
2017-11-10 22:25:19 -0500

    start

--- index.ts
+++ index.ts
@@ -1,9 +1,18 @@
 import * as Rx from 'rxjs';
 
+class Command {
+    constructor(private num: number) {
+
+    }
+
+    execute() {
+        console.log(`Executing ${this.num}`);
+    }
+}
 
 function getObservable(num: number) {
-    return Rx.Observable.create(function (observer: Rx.Observer<any>) {
-        observer.next(num);
+    return Rx.Observable.create(function (observer: Rx.Observer<Command>) {
+        observer.next(new Command(num));
         const x = Math.floor(Math.random() * 100);
 
         setTimeout(() => {
@@ -12,7 +21,7 @@ function getObservable(num: number) {
             } else {
                 observer.error(new Error(`Error with ${num}: ${x}`));
             }
-        }, 1000+x);
+        }, 1000 + x);
     });
 }
 
@@ -23,6 +32,8 @@ const sourceTwo = Rx.Observable.of(4, 5, 6);
 //emit values from sourceOne, when complete, subscribe to sourceTwo
 const example = Rx.Observable.concat(getObservable(1), getObservable(2), getObservable(3), getObservable(4));
 //output: 1,2,3,4,5,6
-const subscribe = example.subscribe(val => console.log('concat:', val), (err) => {
+const subscribe = example.subscribe((val: Command) => {
+    val.execute()
+}, (err) => {
     console.log(err.message);
 });
