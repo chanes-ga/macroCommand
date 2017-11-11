0475c0b77d514d841142da9fce69c4696fcc46fe
Chris Hanes <cah4i@yahoo.com>
2017-11-10 23:09:10 -0500

    start

--- index.ts
+++ index.ts
@@ -1,15 +1,18 @@
 import * as Rx from 'rxjs';
+import * as _ from 'lodash';
+
 /*
-    menuclick call execute on macrocommand
+ menuclick call execute on macrocommand
 
-    macrocommand instantiated with an array of observables that emit a command
-        1) execute => validate selection
-        2) execute => shows dialog
-        3) execute => dialog button click
-        4) validate input
-        5) do action
+ macrocommand instantiated with an array of observables that emit a command
+ 1) execute => validate selection
+ 2) execute => shows dialog
+ 3) execute => dialog button click
+ 4) validate input
+ 5) do action
 
 
+ But what emits the command and completes?
  */
 
 
@@ -20,6 +23,8 @@ class Command {
 
     execute() {
         console.log(`Executing ${this.num}`);
+        return Rx.Observable.empty();
+        // return observable that completes?
     }
 }
 
@@ -43,10 +48,17 @@ const sourceOne = Rx.Observable.of(1, 2, 3);
 //emits 4,5,6
 const sourceTwo = Rx.Observable.of(4, 5, 6);
 //emit values from sourceOne, when complete, subscribe to sourceTwo
-const example = Rx.Observable.concat(getObservable(1), getObservable(2), getObservable(3), getObservable(4));
+
+const commands = [new Command(0), new Command(1), new Command(2)];
+
+
+//const example = Rx.Observable.concat(getObservable(1), getObservable(2), getObservable(3), getObservable(4));
+
+const example = Rx.Observable.concat(_.map(commands, (com: Command) => (com.execute())));
+
 //output: 1,2,3,4,5,6
-const subscribe = example.subscribe((val: Command) => {
-    val.execute()
+const subscribe = example.subscribe((val) => {
+    console.log('got', val);
 }, (err) => {
     console.log(err.message);
 });
