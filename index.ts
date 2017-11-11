d13acc74e510cb1a465b0fcabe81553f9ec5ac97
Chris Hanes <cah4i@yahoo.com>
2017-11-10 22:02:12 -0500

    start

--- index.ts
+++ index.ts
@@ -1,11 +1,17 @@
 import * as Rx from 'rxjs';
 
 
-
 const hello = Rx.Observable.create(function (observer: Rx.Observer<any>) {
     observer.next('Hello');
+
     observer.next('World');
-    observer.complete();
+    const x = Math.random();
+    console.log(x);
+    if (x < 0.5) {
+        observer.complete();
+    } else {
+        observer.error(new Error("bad"));
+    }
 });
 
 //emits 1,2,3
@@ -15,4 +21,6 @@ const sourceTwo = Rx.Observable.of(4, 5, 6);
 //emit values from sourceOne, when complete, subscribe to sourceTwo
 const example = Rx.Observable.concat(sourceOne, hello, sourceTwo);
 //output: 1,2,3,4,5,6
-const subscribe = example.subscribe(val => console.log('Example: Basic concat:', val));
+const subscribe = example.subscribe(val => console.log('Example: Basic concat:', val), (err) => {
+    console.log('b', err);
+});
